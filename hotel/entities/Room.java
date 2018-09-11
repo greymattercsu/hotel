c52d4133de0162f979691e678d50b6743b32f14c
greymattercsu <41317773+greymattercsu@users.noreply.github.com>
2018-09-11 22:26:52 +1000

    class Room
    completed room entity

--- hotel/entities/Room.java
+++ hotel/entities/Room.java
@@ -8,71 +8,80 @@ import hotel.credit.CreditCard;
 import hotel.utils.IOUtils;
 
 public class Room {
-	
-	private enum State {READY, OCCUPIED}
-	
-	int id;
-	RoomType roomType;
-	List<Booking> bookings;
-	State state;
 
-	
-	public Room(int id, RoomType roomType) {
-		this.id = id;
-		this.roomType = roomType;
-		bookings = new ArrayList<>();
-		state = State.READY;
-	}
-	
+    private enum State {
+        READY, OCCUPIED
+    }
 
-	public String toString() {
-		return String.format("Room : %d, %s", id, roomType);
-	}
+    int id;
+    RoomType roomType;
+    List<Booking> bookings;
+    State state;
 
+    public Room(int id, RoomType roomType) {
+        this.id = id;
+        this.roomType = roomType;
+        bookings = new ArrayList<>();
+        state = State.READY;
+    }
 
-	public int getId() {
-		return id;
-	}
-	
-	public String getDescription() {
-		return roomType.getDescription();
-	}
-	
-	
-	public RoomType getType() {
-		return roomType;
-	}
-	
-	public boolean isAvailable(Date arrivalDate, int stayLength) {
-		IOUtils.trace("Room: isAvailable");
-		for (Booking b : bookings) {
-			if (b.doTimesConflict(arrivalDate, stayLength)) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	
-	public boolean isReady() {
-		return state == State.READY;
-	}
+    public String toString() {
+        return String.format("Room : %d, %s", id, roomType);
+    }
 
+    public int getId() {
+        return id;
+    }
 
-	public Booking book(Guest guest, Date arrivalDate, int stayLength, int numberOfOccupants, CreditCard creditCard) {
-		// TODO Auto-generated method stub
-		return null;		
-	}
+    public String getDescription() {
+        return roomType.getDescription();
+    }
 
+    public RoomType getType() {
+        return roomType;
+    }
 
-	public void checkin() {
-		// TODO Auto-generated method stub
-	}
+    public boolean isAvailable(Date arrivalDate, int stayLength) {
+        IOUtils.trace("Room: isAvailable");
+        for (Booking b : bookings) {
+            if (b.doTimesConflict(arrivalDate, stayLength)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public boolean isReady() {
+        return state == State.READY;
+    }
 
-	public void checkout(Booking booking) {
-		// TODO Auto-generated method stub
-	}
+    public Booking book(Guest guest, Date arrivalDate, int stayLength, int numberOfOccupants, CreditCard creditCard) {
+        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub
+        //boolean avail = isAvailable(arrivalDate, stayLength);
 
+        Room r = new Room(this.id, this.roomType);
+
+        Booking booking = new Booking(guest, r, arrivalDate, stayLength, numberOfOccupants, creditCard);
+        bookings.add(booking);
+        return booking;
+    }
+
+    public void checkin() {
+        // TODO Auto-generated method stub
+        if (state != State.READY) {
+            throw new RuntimeException("Room is not ready");
+        }
+        state = State.OCCUPIED;
+    }
+
+    public void checkout(Booking booking) {
+        // TODO Auto-generated method stub
+        if (state != State.OCCUPIED) {
+            throw new RuntimeException("Room is not occupied");
+        }
+        bookings.remove(booking);
+        state = State.READY;
+    }
 
 }
