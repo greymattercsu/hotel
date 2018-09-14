7a615de82c5d4c368db33e265da29951dd48d917
Bijan Dhakal <dhakal.bijan@gmail.com>
2018-09-14 13:57:33 +1000

    Completing the code for checkIn

--- hotel/entities/Booking.java
+++ hotel/entities/Booking.java
@@ -133,7 +133,12 @@ public class Booking {
 
 
 	public void checkIn() {
-		// TODO Auto-generated method stub
+		if (this.state == State.PENDING){
+			room.checkin();
+			this.state = State.CHECKED_IN;
+		}else{
+			throw new RuntimeException();
+	}
 	}
 
 
