a0797396179689970313a636d796fc7878ad101b
greymattercsu <41317773+greymattercsu@users.noreply.github.com>
2018-09-19 14:43:11 +1000

    changing the directory of test fil
    Changing the directory of the test file. Addin in the test class for the checkin method and adding the necessary library in that file

---
+++ test/hotel/checkin/CheckinCTLTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.checkin;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.rules.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import hotel.entities.Booking;
+import hotel.entities.Guest;
+import hotel.entities.Hotel;
+import hotel.entities.Room;
+import hotel.entities.RoomType;
+import hotel.entities.ServiceCharge;
+import hotel.entities.ServiceType;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+public class CheckinCTLTest {
+    
+
+    
+    public CheckinCTLTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+
+    
+
+    
+    
+}
