6e46f81461d117c3ef7842dcaab2af7bd0997504
Bijan Dhakal <dhakal.bijan@gmail.com>
2018-09-22 19:27:46 +1000

    code fix

--- test/BookingJunitTest.java
+++ test/BookingJunitTest.java
@@ -24,81 +24,82 @@ import java.util.Date;
 import java.util.List;
 
 public class BookingJunitTest {
-    
+
     private RoomType roomType = RoomType.SINGLE;
     private Guest guest = new Guest("Bijan DHakal", "some address", 123456789);
-    private Room room = new Room(1,roomType);
+    private Room room = new Room(1, roomType);
     private CreditCard card = new CreditCard(CreditCardType.MASTERCARD, 123456789, 236);
-    private Booking booking = new Booking(guest,room, new Date(2018,12,12),6,1, card);
-    private List<ServiceCharge>charges = new ArrayList<>();
+    private Booking booking = new Booking(guest, room, new Date(2018, 12, 12), 6, 1, card);
+    private List<ServiceCharge> charges = new ArrayList<>();
 
     public BookingJunitTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
 
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    @Rule 
+    @Rule
     public MethodRule watchman = new TestWatchman() {
         public void starting(FrameworkMethod method) {
             System.out.print("[Starting]: " + method.getName());
         }
-        public void failed(Throwable e, FrameworkMethod method){
+
+        public void failed(Throwable e, FrameworkMethod method) {
             System.out.println(" [Failed]\n\t " + e.getMessage());
         }
-        public void succeeded(FrameworkMethod method) { 
+
+        public void succeeded(FrameworkMethod method) {
             System.out.println(" [PASSED]");
         }
     };
-    
-    
+
     @Test
-    public void checkInTest(){
-         booking.checkIn();
-         assertEquals(true, booking.isCheckedIn());
+    public void checkInTest() {
+        booking.checkIn();
+        assertEquals(true, booking.isCheckedIn());
     }
 
     @Test(expected = RuntimeException.class)
-    public void checkInTestException(){
-         booking.checkIn();
-         booking.checkIn();
+    public void checkInTestException() {
+        booking.checkIn();
+        booking.checkIn();
     }
 
     @Test(expected = RuntimeException.class)
-    public void checkInTestExceptionBookingStateCheckout(){
-         booking.checkOut();
-         booking.checkIn();
+    public void checkInTestExceptionBookingStateCheckout() {
+        booking.checkOut();
+        booking.checkIn();
     }
-    
+
     @Test
-    public void addServiceChargeTest(){
-        
+    public void addServiceChargeTest() {
+
         ServiceType serviceType = ServiceType.ROOM_SERVICE;
         int cost = 100;
         ServiceCharge serviceCharge = new ServiceCharge(serviceType, cost);
-		charges.add(serviceCharge);
-        
+        charges.add(serviceCharge);
+
         booking.checkIn();
         booking.addServiceCharge(serviceType, cost);
         assertEquals(charges, booking.getCharges());
     }
-    
+
     @Test(expected = RuntimeException.class)
-    public void addServiceChargeTestException(){
+    public void addServiceChargeTestException() {
         ServiceType serviceType = ServiceType.ROOM_SERVICE;
         int cost = 100;
         ServiceCharge serviceCharge = new ServiceCharge(serviceType, cost);
@@ -107,39 +108,39 @@ public class BookingJunitTest {
 
         booking.addServiceCharge(serviceType, cost);
         assertEquals(charges, booking.getCharges());
-        
+
     }
 
     @Test
-    public void addServiceChargeTestDiffrentService(){
-        
+    public void addServiceChargeTestDiffrentService() {
+
         ServiceType serviceType1 = ServiceType.ROOM_SERVICE;
         ServiceType serviceType2 = ServiceType.BAR_FRIDGE;
         int cost = 100;
         ServiceCharge serviceCharge = new ServiceCharge(serviceType1, cost);
-		charges.add(serviceCharge);
-        
+        charges.add(serviceCharge);
+
         booking.checkIn();
         booking.addServiceCharge(serviceType2, cost);
         assertNotSame(charges, booking.getCharges());
     }
 
     @Test
-    public void addServiceChargeTestDiffrentCost(){
-        
+    public void addServiceChargeTestDiffrentCost() {
+
         ServiceType serviceType = ServiceType.ROOM_SERVICE;
         int cost = 100;
         ServiceCharge serviceCharge = new ServiceCharge(serviceType, cost);
-		charges.add(serviceCharge);
-        
+        charges.add(serviceCharge);
+
         booking.checkIn();
         booking.addServiceCharge(serviceType, 150);
         assertNotSame(charges, booking.getCharges());
     }
 
     @Test
-    public void addServiceChargeTestTwoServiceSame(){
-        
+    public void addServiceChargeTestTwoServiceSame() {
+
         ServiceType serviceTypeFirst = ServiceType.ROOM_SERVICE;
         ServiceType serviceTypeSecond = ServiceType.RESTAURANT;
         int costFirst = 100;
@@ -147,17 +148,17 @@ public class BookingJunitTest {
         ServiceCharge serviceCharge = new ServiceCharge(serviceTypeFirst, costFirst);
         charges.add(serviceCharge);
         serviceCharge = new ServiceCharge(serviceTypeSecond, costSecond);
-		charges.add(serviceCharge);
-        
+        charges.add(serviceCharge);
+
         booking.checkIn();
         booking.addServiceCharge(serviceTypeFirst, costFirst);
         booking.addServiceCharge(serviceTypeSecond, costSecond);
         assertEquals(charges, booking.getCharges());
     }
-    
+
     @Test
-    public void addServiceChargeTestTwoServiceDiffrent(){
-        
+    public void addServiceChargeTestTwoServiceDiffrent() {
+
         ServiceType serviceTypeFirst = ServiceType.ROOM_SERVICE;
         ServiceType serviceTypeSecond = ServiceType.RESTAURANT;
         int costFirst = 100;
@@ -165,24 +166,24 @@ public class BookingJunitTest {
         ServiceCharge serviceCharge = new ServiceCharge(serviceTypeFirst, costFirst);
         charges.add(serviceCharge);
         serviceCharge = new ServiceCharge(serviceTypeSecond, costSecond);
-		charges.add(serviceCharge);
-        
+        charges.add(serviceCharge);
+
         booking.checkIn();
         booking.addServiceCharge(serviceTypeFirst, costFirst);
         booking.addServiceCharge(serviceTypeFirst, costSecond);
         assertNotSame(charges, booking.getCharges());
     }
 
-    
     @Test(expected = RuntimeException.class)
-    public void checkoutTestException(){
+    public void checkoutTestException() {
         booking.checkOut();
-    }   
+    }
+
     @Test
-    public void checkoutTest(){
+    public void checkoutTest() {
         booking.checkIn();
         booking.checkOut();
         assertEquals(true, booking.isCheckedOut());
     }
-    
+
 }
