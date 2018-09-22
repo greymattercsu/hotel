718364ad71120aaead9775e25bc7d0fe527e5177
Bijan Dhakal <dhakal.bijan@gmail.com>
2018-09-22 19:26:03 +1000

    update testrunner for integration test

--- test/TestRunner.java
+++ test/TestRunner.java
@@ -8,9 +8,22 @@ public class TestRunner {
     static JUnitCore junitCore;
 
     public static void main(String[] args) {
-        System.out.println("Running Unit Test.");
-        Result result = JUnitCore.runClasses(BookingJunitTest.class);
-        System.out.println("[Test Executed]: " + result.getRunCount());
-        System.out.println("[Test result]: " + result.wasSuccessful());      
+        if (args.length == 0) {
+            System.out.println("Please pass valid arguments 'Unit' or 'integration'");
+        } else {
+            if (args[0].equalsIgnoreCase("unit")) {
+                System.out.println("Running Unit Test.");
+                Result result = JUnitCore.runClasses(BookingJunitTest.class);
+                System.out.println("[Test Executed]: " + result.getRunCount());
+                System.out.println("[Test result]: " + result.wasSuccessful());
+
+            } else if (args[0].equalsIgnoreCase("integration")) {
+                Result result2 = JUnitCore.runClasses(BookingCTLTest.class);
+                System.out.println("[Test Executed]: " + result2.getRunCount());
+                System.out.println("[Test result]: " + result2.wasSuccessful());
+            } else {
+                System.out.println("Please pass valid arguments 'Unit' or 'integration'");
+            }
+        }
     }
-}  	
+}
